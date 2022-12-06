67d1809eba06dbf17c0690851c8c20370b388dbf
unknown <mosunov.1982@inbox.ru>
2022-12-06 09:54:28 +0300

    list

--- pagelogin.js
+++ pagelogin.js
@@ -4,36 +4,65 @@
 
             let content = document.querySelector(".content");
 
-            let text = document.createElement("div");
-            text.append(document.createTextNode("вход"));
-            text.classList.add("formName");
-
-            let EmailFiled = document.createElement("input");
-            EmailFiled.classList.add("formEmail");
-
-            let text2 = document.createElement("div");
-            text2.append(document.createTextNode("E-mail"));
-            text2.classList.add("formName2");
-
-            let PasswordFiled = document.createElement("input");
-            PasswordFiled.classList.add("formPassword");
-
-            let text3 = document.createElement("div");
-            text3.append(document.createTextNode("Пароль"));
-            text3.classList.add("formName3");
-
-            let logbutton = document.createElement("button");
-            logbutton.append(document.createTextNode("Войти"));
-            logbutton.classList.add("logbutton");
-
-            let registrbutton = document.createElement("button");
-            registrbutton.append(document.createTextNode("Зарегистрироваться"));
-            registrbutton.classList.add("registrbutton");
+            let text = create_text_login();
+            let EmailFiled = create_email_filed();
+            let text2 = create_text_email();
+            let PasswordFiled = create_password_filed();
+            let text3 = create_text_password();
+            let logbutton = create_button_login();
+            let registrbutton = create_button_registr();
 
             registrbutton.addEventListener("click", GoToRegistr)
 
             content.append(text, EmailFiled, text2, PasswordFiled, text3, logbutton, registrbutton);
         }
+    }
+
+    function create_text_login() {
+        let text = document.createElement("div");
+        text.append(document.createTextNode("Вход"));
+        text.classList.add("formName");
+        return text;
+    }
+
+    function create_email_filed() {
+        let EmailFiled = document.createElement("input");
+        EmailFiled.classList.add("formEmail");
+        return EmailFiled;
+    }
+
+    function create_text_email() {
+        let text2 = document.createElement("div");
+        text2.append(document.createTextNode("E-mail"));
+        text2.classList.add("formName2");
+        return text2;
+    }
+
+    function create_password_filed() {
+        let PasswordFiled = document.createElement("input");
+        PasswordFiled.classList.add("formPassword");
+        return PasswordFiled;
+    }
+
+    function create_text_password() {
+        let text3 = document.createElement("div");
+        text3.append(document.createTextNode("Пароль"));
+        text3.classList.add("formName3");
+        return text3;
+    }
+
+    function create_button_login() {
+        let logbutton = document.createElement("button");
+        logbutton.append(document.createTextNode("Войти"));
+        logbutton.classList.add("logbutton");
+        return logbutton;
+    }
+
+    function create_button_registr() {
+        let registrbutton = document.createElement("button");
+        registrbutton.append(document.createTextNode("Зарегистрироваться"));
+        registrbutton.classList.add("registrbutton");
+        return registrbutton;
     }
 
     function GoToRegistr() {
--- pageregistr.js
+++ pageregistr.js
@@ -4,51 +4,97 @@
 
             let content = document.querySelector(".content");
 
-            let text4 = document.createElement("div");
-            text4.append(document.createTextNode("регистрация"));
-            text4.classList.add("formName4");
-
-            let EmailFiled2 = document.createElement("input");
-            EmailFiled2.classList.add("formEmail2");
-
-            let text5 = document.createElement("div");
-            text5.append(document.createTextNode("E-mail"));
-            text5.classList.add("formName5");
-
-            let TelephFiled = document.createElement("input");
-            TelephFiled.classList.add("formTeleph");
-
-            let text6 = document.createElement("div");
-            text6.append(document.createTextNode("Телефон"));
-            text6.classList.add("formName6");
-
-            let PasswordFiled2 = document.createElement("input");
-            PasswordFiled2.classList.add("formPassword2");
-
-            let text7 = document.createElement("div");
-            text7.append(document.createTextNode("Пароль"));
-            text7.classList.add("formName7");
-
-            let ConfirmPasswordFiled = document.createElement("input");
-            ConfirmPasswordFiled.classList.add("formConfirmPassword");
-
-            let text8 = document.createElement("div");
-            text8.append(document.createTextNode("Подтверждение пароля"));
-            text8.classList.add("formName8");
-
-            let regbutton = document.createElement("button");
-            regbutton.append(document.createTextNode("Зарегистрироваться"));
-            regbutton.classList.add("formregbutton");
-
-            let loginbutton = document.createElement("button");
-            loginbutton.append(document.createTextNode("Войти"));
-            loginbutton.classList.add("formloginbutton");
+            let text4 = create_text_registr();
+            let EmailFiled2 = create_email_filed();
+            let text5 = create_text_email();
+            let TelephFiled = create_teleph_filed();
+            let text6 = create_text_teleph();
+            let PasswordFiled2 = create_password_filed();
+            let text7 = create_text_password();
+            let ConfirmPasswordFiled = create_confirmpassword_filed();
+            let text8 = create_text_confirmpassword();
+            let regbutton = create_button_login();
+            let loginbutton = create_button_registr();
 
             loginbutton.addEventListener("click", GoToLogin)
 
             content.append(text4, EmailFiled2, text5, TelephFiled, text6, PasswordFiled2, text7, ConfirmPasswordFiled, text8, regbutton, loginbutton);
         }
     }
+
+    function create_text_registr() {
+        let text4 = document.createElement("div");
+        text4.append(document.createTextNode("Регистрация"));
+        text4.classList.add("formName4");
+        return text4;
+    }
+
+    function create_email_filed() {
+        let EmailFiled2 = document.createElement("input");
+        EmailFiled2.classList.add("formEmail2");
+        return EmailFiled2;
+    }
+
+    function create_text_email() {
+        let text5 = document.createElement("div");
+        text5.append(document.createTextNode("E-mail"));
+        text5.classList.add("formName5");
+        return text5;
+    }
+
+    function create_teleph_filed() {
+        let TelephFiled = document.createElement("input");
+        TelephFiled.classList.add("formTeleph");
+        return TelephFiled;
+    }
+
+    function create_text_teleph() {
+        let text6 = document.createElement("div");
+        text6.append(document.createTextNode("Телефон"));
+        text6.classList.add("formName6");
+        return text6;
+    }
+
+    function create_password_filed() {
+        let PasswordFiled2 = document.createElement("input");
+        PasswordFiled2.classList.add("formPassword2");
+        return PasswordFiled2;
+    }
+
+    function create_text_password() {
+        let text7 = document.createElement("div");
+        text7.append(document.createTextNode("Пароль"));
+        text7.classList.add("formName7");
+        return text7;
+    }
+
+    function create_confirmpassword_filed() {
+        let ConfirmPasswordFiled = document.createElement("input");
+        ConfirmPasswordFiled.classList.add("formConfirmPassword");
+        return ConfirmPasswordFiled;
+    }
+
+    function create_text_confirmpassword() {
+        let text8 = document.createElement("div");
+        text8.append(document.createTextNode("Подтверждение пароля"));
+        text8.classList.add("formName8");
+        return text8;
+    }
+
+    function create_button_login() {
+        let regbutton = document.createElement("button");
+        regbutton.append(document.createTextNode("Зарегистрироваться"));
+        regbutton.classList.add("formregbutton");
+        return regbutton;
+    }
+
+    function create_button_registr() {
+        let loginbutton = document.createElement("button");
+        loginbutton.append(document.createTextNode("Войти"));
+        loginbutton.classList.add("formloginbutton");
+        return loginbutton;
+    }
+
     function GoToLogin() {
         document.querySelector(".content").innerHTML = "";
         app.PageLogin.draw();
